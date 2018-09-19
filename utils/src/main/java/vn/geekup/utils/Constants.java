6e4462a5d8e630289d973a8aa00ec80cdb63518d
Quoc Trung <quoctrung66@gmail.com>
2018-09-19 09:23:45 +0700

    add joda time lib

--- utils/src/main/java/vn/geekup/utils/Constants.java
+++ utils/src/main/java/vn/geekup/utils/Constants.java
@@ -13,12 +13,12 @@ public class Constants {
     public static final String UTF_16BE = "UTF_16BE";
     public static final String UTF_16LE = "UTF_16LE";
     public static final String UTF_8 = "UTF_8";
-    public static final String DASH_STRING = "--";
 
     @Retention(RetentionPolicy.SOURCE)
     @StringDef({ISO_8859_1, US_ASCII, UTF_16, UTF_16BE, UTF_16LE, UTF_8})
     public @interface Endcoding {}
 
     public static final String PERCENT_TAG = "%";
+    public static final String DASH_STRING = "--";
 
 }
